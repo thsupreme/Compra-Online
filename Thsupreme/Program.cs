5c81354a4fe822daa9a09a3b7540670167043086
thiia <thiia@DESKTOP-0K258DB>
2022-10-11 15:26:05 -0300

    Adicionar arquivos de projeto.

---
+++ Thsupreme/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Thsupreme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------- Compra Online ---------------");
+            Console.WriteLine("");
+            Console.WriteLine("--------------- Dados do Usuario ---------------");
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite seu Saldo: ");
+            double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);         
+            Console.WriteLine("--------------- Dados do Produdo ---------------");
+            Console.WriteLine("");
+            Console.Write("Digite o nome do Produto: ");
+            string nomeProduto = Console.ReadLine();
+            Console.Write("Digite o valor do Produto: ");
+            double valorProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double saldoFinal = saldo - valorProduto;
+            bool compraRealizada = saldoFinal >= 0;
+
+            if (compraRealizada)
+            {
+                Console.WriteLine("Compra Realizada com Sucesso !!");
+            }
+
+            else
+            {
+                Console.WriteLine("Compra Cancelada");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
